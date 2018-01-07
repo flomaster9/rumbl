3ba062e9109c706dd1045ddb7fd597a565762df9
Daniel <flomaster9@mail.ru>
2018-01-08 02:42:14 +0300

    Add CRD operations for user

--- lib/rumbl_web/models/user.ex
+++ lib/rumbl_web/models/user.ex
@@ -1,3 +1,20 @@
 defmodule Rumbl.User do
-  defstruct [:id, :login, :email, :password]
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :login, :string
+    field :email, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+
+    timestamps()
+    end
+
+    def changeset(model, params \\ %{}) do
+      model
+        |> cast(params, ~w(login email), [])
+        |> validate_required([:login, :email])
+        |> validate_length(:login, min: 6, max: 20)
+    end
 end
--- lib/rumbl_web/router.ex
+++ lib/rumbl_web/router.ex
@@ -17,8 +17,7 @@ defmodule RumblWeb.Router do
     pipe_through :browser # Use the default browser stack
     get "/", PageController, :index
 
-    get "/users/:id", UserController, :show
-    get "/users", UserController, :index
+    resources "/users", UserController, only: [:index, :show, :new, :create]
   end
 
   # Other scopes may use custom stacks.
